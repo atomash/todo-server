068a17548210118c666129f537ac38c30bd52780
Tomash <tomash@MacBook-Pro-Andrii.local>
2018-02-22 19:32:03 +0200

    up

--- src/api/models/task.js
+++ src/api/models/task.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
     title: { type: String, required: true },
-    priority: { type: Number, required: true }
+    priority: { type: String, required: true },
+    prioritylvl: { type: Number, required: true }
 });
 
 export default mongoose.model('Todo', schema);
