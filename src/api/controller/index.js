b626e18ef3ef88f9a9bdde494292cabf7be9e177
Tomash <tomash@MacBook-Pro-Andrii.local>
2018-02-25 12:13:08 +0200

    add update function

--- src/api/controller/index.js
+++ src/api/controller/index.js
@@ -28,11 +28,23 @@ exports.getList = async (req, res) => {
 exports.removeTask = async (req, res) => {
     try{
         await Task.remove({ _id: req.params.id })
-        // let todoList = await Task.find();
-        // res.status(200).json({ 
-        //     todo:sortList(todoList)
-        // });
         res.status(200).json({ message: "ok" });
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }   
+}
+
+exports.updateTask = async (req, res) => {
+    try{
+        const updatedTask = {
+            title: req.body.title,
+            priority: req.body.priority 
+        }
+        const upTask = await Task.update(
+            { _id: req.params.id }, 
+            { $set : updatedTask}
+        )
+        res.status(200).json({ task: updatedTask });
     } catch (error) {
         res.status(500).json({ message: error });
     }   
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { add, getList, removeTask } from './controller';
+import { add, getList, removeTask, updateTask } from './controller';
 
 const router = express.Router();
 
 router.get('/todo', getList);
 router.post('/todo', add);
 router.delete('/todo/:id', removeTask);
+router.put('/todo/:id', updateTask);
 
 export default router;
